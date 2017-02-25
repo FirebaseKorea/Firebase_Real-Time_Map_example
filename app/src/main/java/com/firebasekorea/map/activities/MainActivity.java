55ee7f900cdb6c20a60f5a5a60028e5f742babb1
Namhoon Kim <namhoonkim@Namhoonui-MacBook-Pro.local>
2017-02-25 15:58:58 +0900

    naver map test

--- app/src/main/java/com/firebasekorea/map/activities/MainActivity.java
+++ app/src/main/java/com/firebasekorea/map/activities/MainActivity.java
@@ -4,23 +4,38 @@ import android.os.Bundle;
 
 import com.firebasekorea.map.R;
 import com.firebasekorea.map.utils.ToastUtil;
+import com.nhn.android.maps.NMapActivity;
+import com.nhn.android.maps.NMapView;
 
 /**
  * Created by namhoonkim on 25/02/2017.
  */
 
-public class MainActivity extends BaseActivity {
+public class MainActivity extends NMapActivity {
     private final String TAG = MainActivity.class.getSimpleName();
+
 
     private long pressTime = 0;
 
     /* View Component */
+    private NMapView mMapView;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        mMapView = new NMapView(this);
+        setContentView(mMapView);
+        mMapView.setClientId(getString(R.string.naver_client_id));
+        mMapView.setClickable(true);
+        mMapView.setEnabled(true);
+        mMapView.setFocusable(true);
+        mMapView.setFocusableInTouchMode(true);
+        mMapView.requestFocus();
+
+//        setContentView(R.layout.activity_main);
+
+        setTitle("Firebase Real-Time Map Example");
 
         init();
     }
