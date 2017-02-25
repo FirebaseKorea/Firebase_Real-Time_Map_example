528bd83489159febecbddc4dd0c55fc9eab53ccf
Namhoon Kim <namhoonkim@Namhoonui-MacBook-Pro.local>
2017-02-25 15:25:53 +0900

    update user util

--- app/src/main/java/com/firebasekorea/map/utils/UserUtil.java
+++ app/src/main/java/com/firebasekorea/map/utils/UserUtil.java
@@ -5,11 +5,34 @@ package com.firebasekorea.map.utils;
  */
 
 public class UserUtil {
+
+    public static void saveUserFirebaseUid(String firebaseUid) {
+        SharedPreferenceUtil.getInstance().putString(SharedPreferenceUtil.FIREBASE_UID, firebaseUid);
+    }
+
+    public static String loadUserFirebaseUid() {
+        return SharedPreferenceUtil.getInstance().getString(SharedPreferenceUtil.FIREBASE_UID, null);
+    }
+
+    public static void saveUserEmail(String userEmail) {
+        SharedPreferenceUtil.getInstance().putString(SharedPreferenceUtil.USER_EMAIL, userEmail);
+    }
+    public static String loadUserEmail() {
+        return SharedPreferenceUtil.getInstance().getString(SharedPreferenceUtil.USER_EMAIL, null);
+    }
+
     public static void saveUserName(String userName) {
-//        SharedPreferenceUtil.getInstance().putString(SharedPreferenceUtil.USER_NAME, userName);
+        SharedPreferenceUtil.getInstance().putString(SharedPreferenceUtil.USER_NAME, userName);
     }
     public static String loadUserName() {
-//        return SharedPreferenceUtil.getInstance().getString(SharedPreferenceUtil.USER_NAME, null);
-        return "";
+        return SharedPreferenceUtil.getInstance().getString(SharedPreferenceUtil.USER_NAME, null);
     }
+
+    public static void saveUserProfilePictureUrl(String profilePictureUrl) {
+        SharedPreferenceUtil.getInstance().putString(SharedPreferenceUtil.USER_PROFILE_URL, profilePictureUrl);
+    }
+    public static String loadUserProfilePictureUrl() {
+        return SharedPreferenceUtil.getInstance().getString(SharedPreferenceUtil.USER_PROFILE_URL, null);
+    }
+
 }
