35cc63fe5cefecf063ed4dfe2d4f06fea2418095
Namhoon Kim <namhoonkim@Namhoonui-MacBook-Pro.local>
2017-02-26 18:02:13 +0900

    add floating button in main activity

--- app/src/main/java/com/firebasekorea/map/activities/MainActivity.java
+++ app/src/main/java/com/firebasekorea/map/activities/MainActivity.java
@@ -3,6 +3,9 @@ package com.firebasekorea.map.activities;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.util.Log;
+import android.view.View;
 import android.widget.TextView;
 
 import com.firebasekorea.map.R;
@@ -15,7 +18,7 @@ import com.nhn.android.maps.NMapView;
  * Created by namhoonkim on 25/02/2017.
  */
 
-public class MainActivity extends BaseActivity {
+public class MainActivity extends BaseActivity implements View.OnClickListener {
     private final String TAG = MainActivity.class.getSimpleName();
 
 
@@ -25,6 +28,7 @@ public class MainActivity extends BaseActivity {
     private TextView mLongitudeTextView;
     private TextView mLatitudeTextView;
 
+    private FloatingActionButton mFloatingActionButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,10 +55,34 @@ public class MainActivity extends BaseActivity {
     private void init() {
         mLongitudeTextView = (TextView) findViewById(R.id.main_longitude);
         mLatitudeTextView = (TextView) findViewById(R.id.main_latitude);
+        mFloatingActionButton = (FloatingActionButton) findViewById(R.id.main_floating);
+        mFloatingActionButton.setOnClickListener(this);
     }
 
     public void updateGeoPoint(double longitude, double latitude) {
         mLongitudeTextView.setText("경도 : " + longitude);
         mLatitudeTextView.setText("위도 : " + latitude);
     }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.main_floating:
+                onClickFloatingButton();
+                break;
+        }
+    }
+
+    private void onClickFloatingButton() {
+        String longtitude = mLongitudeTextView.getText().toString().substring(5);
+        String latitude = mLatitudeTextView.getText().toString().substring(5);
+
+        if (longtitude == null || longtitude.length() < 1) {
+            ToastUtil.makeShortToast(this, "경도 및 위도가 선택되지 않았습니다");
+        }
+
+        Log.d(TAG, "longtitude : " + longtitude);
+        Log.d(TAG, "latitude   : " + latitude);
+    }
+
 }
